1dd2dbc7e687f54cdc38c948cc9247dfe9f2bb4a
Nuno Marques <ositaka@gmail.com>
2020-02-06 18:07:55 +0100

    StoryGalleryPreview2

---
+++ src/cms/preview-templates/StoryGalleryPreview2.js
@@ -0,0 +1,31 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import { BlogPostTemplate } from '../../templates/blog-post'
+
+const StoryGalleryPreview2 = ({ entry, widgetFor }) => {
+	const data = entry.getIn(['data']).toJS()
+
+	if (data) {
+		return (
+			<>
+				<BlogPostTemplate gallerySwiper={data.gallery || { images: [] }} />
+			</>
+		)
+	} else {
+		return <div>Loading...</div>
+	}
+}
+
+StoryGalleryPreview2.propTypes = {
+	entry: PropTypes.shape({
+		getIn: PropTypes.func
+	}),
+	widgetFor: PropTypes.func,
+
+	// test - maybe not needed
+	gallery: PropTypes.shape({
+		images: PropTypes.array
+	})
+}
+
+export default StoryGalleryPreview2
